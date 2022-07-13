8c8fd76acbd16fbd35565a23051f65dafba4afb0
Rachel Whiteley <ladywy0929@outlook.com>
2022-07-12 20:06:12 -0400

    add JS variables to change section headers played around with borders and colors centered headings

---
+++ Index.js
@@ -0,0 +1,11 @@
+let skillA = "Web Development";
+document.getElementById("skilla").innerHTML = skillA;
+
+let skillB = "TBD";
+document.getElementById("skillb").innerHTML = skillB;
+
+let skillC = "TBD";
+document.getElementById("skillc").innerHTML = skillC;
+
+let skillD = "TBD";
+document.getElementById("skilld").innerHTML = skillD;
