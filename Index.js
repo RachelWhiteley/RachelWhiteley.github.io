8c9152d239de78d01e01bb596cc0a525d18bcbc3
Rachel Whiteley <ladywy0929@outlook.com>
2022-07-24 10:24:52 -0400

    added submit button and beginning of javascript email validation code

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 let skillA = "Web Development";
 document.getElementById("skilla").innerHTML = skillA;
 
-let skillB = "TBD";
+let skillB = "Language";
 document.getElementById("skillb").innerHTML = skillB;
 
 let skillC = "TBD";
@@ -9,3 +9,20 @@ document.getElementById("skillc").innerHTML = skillC;
 
 let skillD = "TBD";
 document.getElementById("skilld").innerHTML = skillD;
+
+function ValidateEmail(inputText)
+{
+let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
