287dc4c3f2f5611b379942ab1352a9dc7603f371
Rachel Whiteley <ladywy0929@outlook.com>
2022-07-24 18:08:57 -0400

    removed some Javascript that was beyond me changed and added colors padding and some flex changes

--- Index.js
+++ Index.js
@@ -10,19 +10,3 @@ document.getElementById("skillc").innerHTML = skillC;
 let skillD = "TBD";
 document.getElementById("skilld").innerHTML = skillD;
 
-function ValidateEmail(inputText)
-{
-let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(inputText.value.match(mailformat))
-{
-alert("Valid email address!");
-document.form1.text1.focus();
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-document.form1.text1.focus();
-return false;
-}
-}
